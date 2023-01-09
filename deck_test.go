5eb41a3e5664e8ea2f605fc6533b6c73fc5f9924
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2023-01-09 19:04:53 +0330

    Testing the read and write to file

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+    "testing"
+    "os"
+)
 
 func TestNewDeck(t *testing.T) {
     d := newDeck()
@@ -16,4 +19,20 @@ func TestNewDeck(t *testing.T) {
     if d[len(d) - 1] != "King of Clubs" {
         t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d) - 1])
     }
+}
+// Testing the read and write to file
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+    os.Remove("_deckTesting")
+
+    deck := newDeck()
+
+    deck.saveToFile("_deckTesting")
+
+    loadedDeck := newDeckFromFile("_deckTesting")
+
+    if len(loadedDeck) != 52 {
+        t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+    }
+
+    os.Remove("_deckTesting")
 }
